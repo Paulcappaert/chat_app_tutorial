6a4657cf3e6ab972feec1c921fb377b9205182ae
Paul Cappaert <paul.cappaert@gmail.com>
2020-02-09 10:33:25 -0500

    secret key secure

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%o0hn77@^@xldkj)j=s8!n4#6&2x*zrfw=#h2oe=6nyeq1*%j*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
